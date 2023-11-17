8db6de6f7842dc42e3f1ecb2a42db67c920a5de5
Outi Ravila <outi.ravila@gmail.com>
2023-11-17 14:40:13 +0200

    Initial commit

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+// Home.js
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome!</h1>
+      <p>Here you can book a slot to use the washing machine</p>
+      <p>
+        Please select date and time below. Each slot is for 1 hour.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
