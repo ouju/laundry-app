a35a071822632c2f443b5d92ffce6803ffa60176
Outi Ravila <outi.ravila@gmail.com>
2023-11-22 12:38:15 +0200

    show upcoming reservations in My Bookings page

--- src/pages/MyBookings.js
+++ src/pages/MyBookings.js
@@ -1,16 +1,37 @@
 // MyBookings.js
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import bookingsData from '../data/bookings.json';
 
 function MyBookings() {
   const location = useLocation();
   const selectedSlot = location.state?.selectedSlot;
+  const userId = "1010"; // Current user's ID
+  const userData = bookingsData[userId];
+
+  if (!userData) {
+    return (
+      <div>
+        <h2>My Bookings</h2>
+        <p>No bookings found for user ID: {userId}</p>
+      </div>
+    );
+  }
+
+  const { booked } = userData;
 
   return (
     <div>
       <h2>My Bookings</h2>
       {selectedSlot && <p>Time Slot successfully reserved: {selectedSlot}</p>}
       <h3>Upcoming time slots</h3>
+      <ul>
+        {booked.map((booking, index) => (
+          <li key={index}>
+            {`Date: ${booking.date}, Time: ${booking.time}, Machine: ${booking.machine}`}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
